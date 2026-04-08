350b7da7e871d7d7df317ea078cfbd63ecc5302d
infinity-789 <abhay.shankar3003@gmail.com>
2026-04-08 18:50:39 +0530

    first commit

---
+++ src/test/java/com/devops/lab/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.devops.lab;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+
+    void testAddition() {
+        assertEquals(4, calc.add(2, 2));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(0, calc.sub(2, 2));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(4, calc.mul(2, 2));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(1, calc.div(2, 2));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calc.div(10,0));
+
+        assertEquals("Cannot divide by zero", exception.getMessage());
+    }
+}
